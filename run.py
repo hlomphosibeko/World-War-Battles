ec9eeac0c5ff14fbfadf7e39530376a3ae50cd4d
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-09-26 17:32:32 +0000

    Add class and docstring, create populate board function

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
+#Legend
+# X for placing ship and hit battleship
+# '-' for missed shot
+
 from random import randint
+
+class Board:
+    """
+    This is the main board. It sets the board size, numbers of ships, and player's name. It also allows the player to play against the computer.
+    """
+    def populate_board(board):
+        pass
+
 
 
