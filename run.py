ae7fb63325738e7d01b620466fa463a6d440c2b7
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-09-26 19:09:21 +0000

    Create __init__ method, add attributes insided

--- run.py
+++ run.py
@@ -8,6 +8,16 @@ class Board:
     """
     This is the main board. It sets the board size, numbers of ships, and player's name. It also allows the player to play against the computer.
     """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+        
     def populate_board(board):
         pass
 
