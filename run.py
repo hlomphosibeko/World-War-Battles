a190567e624a7c43d95b0a727355d707a32f83b0
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-09-26 19:41:18 +0000

    Create new game function, print welcome message, call the function inside the class, delete redundant code

--- run.py
+++ run.py
@@ -4,6 +4,8 @@
 
 from random import randint
 
+scores = {"computer": 0, "player": 0}
+
 class Board:
     """
     This is the main board. It sets the board size, numbers of ships, and player's name. It also allows the player to play against the computer.
@@ -17,9 +19,18 @@ class Board:
         self.guesses = []
         self.ships = []
 
-        
-    def populate_board(board):
-        pass
 
+    def new_game():
+        """
+        Starts a new game. Sets the board size and number of the ships, resets the scores and initialises the boards.
+        """
+
+        size = 8
+        num_ships = 5
+        scores["computer"] = 0
+        scores["player"] = 0
+        print("Welcome to WORLD WAR BATTLESHIPS!!!")
+        
+    new_game()
 
 
