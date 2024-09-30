9c31150d96d6fd278e26f60879f99d909ef9684a
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-09-30 10:46:16 +0000

    Create a function to run the game, add an instance to all methods inside the class, remove unwanted code

--- run.py
+++ run.py
@@ -15,48 +15,64 @@ class TheBoards:
         self.num_of_ships = num_of_ships
         self.player_name = player_name
         self.game_type = game_type
-        self.TheBoards = [['!' for x in range(size)] for y in range(size)]
+        self.my_board = [['!' for x in range(size)] for y in range(size)]
         self.my_guesses = []
         self.my_ships = []
 
 
     def print(self):
-        for x in self.TheBoards:
-            print("".join(x))
+        for x in self.my_board:
+            print("   ".join(x))
 
     
     def guess_generator(self,x,y):
         """
         This function inserts 'X' inside the board on coordinates x and y. It creates a tuple containing x and y, and appends it to the my_guesses list.
         """
-    
-        my_guesses.append((x,y)) 
-        if (x,y) in my_ships:
-            my_board[x][y] = 'X'
+        self.my_guesses.append((x,y)) 
+        if (x,y) in self.my_ships:
+            self.my_board[x][y] = 'X'
             print("Hit")
             return "Hit"
         else:
             print("Missed")
-            my_board[x][y] = '-'
+            self.my_board[x][y] = '-'
             return "Missed"
 
 
-    def ship_generator(x,y):
+    def ship_generator(self,x,y):
         """
         This function inserts '&' as a ship inside the board on coordinates x and y. It creates a tuple containing x and y and appends it to the my_ships list.
         """
-        my_board[x][y] = '&'
-        my_ships.append((x,y))    
+        self.my_board[x][y] = '&'
+        self.my_ships.append((x,y))    
         return
 
 
-    def random_number(size):
+    def random_number(self,size):
         """
         This function generates a random number between 0 and size.
         """
         return randint(0,size)
-    
 
+
+def run_game():
+    name = input("Please insert your name: \n")
+    print(f"Hello {name}, Welcome to World War Battles!!")
+    ndalo_board = TheBoards(8,5,'Ndalo','Ndalo')
+    ndalo_board.ship_generator(ndalo_board.random_number(int(input("Insert size: "))),ndalo_board.random_number(int(input("Insert size: "))))
+
+run_game()  
+#TheBoards.print(TheBoards(8,5,'Hlompho','Hlompho'))
+#ndalo_board = TheBoards(8,5,'Ndalo','Ndalo')
+#ndalo_board.guess_generator(2,2)
+#ndalo_board.print()
+#hlompho_board = TheBoards(8,5,'Hlompho','Hlompho')
+#hlompho_board.ship_generator(4,2)
+#hlompho_board.guess_generator(4,2)
+#hlompho_board.print()
+#b = TheBoards.ship_generator(TheBoards(8,5,'Hlompho','Hlompho'),2,5)
+#TheBoards.print(b)
 #my_board = [['!','!','!','!','!'],['!','!','!','!','!'],['!','!','!','!','!'],['!','!','!','!','!'],['!','!','!','!','!']]
 #my_list[0][4] = 'a'
 #print(my_list)
@@ -67,47 +83,8 @@ class TheBoards:
 #my_list[4][4] = 'X'
 #print(my_list)
 
-#my_guesses = []
-#my_ships = []
+
 #my_guesses.append((0, 0))
-
-#def create_board(z):
-    """
-    This function generates the board z number of rows and z number of columns.
-    """
-   # [['!' for x in range(z)] for y in range(z)]
-    #return 
-
-
-"""def guess_generator(x,y):
-    """
-    This function inserts 'X' inside the board on coordinates x and y. It creates a tuple containing x and y, and appends it to the my_guesses list.
-    """
-    my_guesses.append((x,y)) 
-    if (x,y) in my_ships:
-        my_board[x][y] = 'X'
-        print("Hit")
-        return "Hit"
-    else:
-        print("Missed")
-        my_board[x][y] = '-'
-        return "Missed" """
-    
-
-"""def ship_generator(x,y):
-    """
-    This function inserts '&' as a ship inside the board on coordinates x and y. It creates a tuple containing x and y and appends it to the my_ships list.
-    """
-    my_board[x][y] = '&'
-    my_ships.append((x,y))    
-    return """
-
-"""def random_number(size):
-    """
-    This function generates a random number between 0 and size.
-    """
-    return randint(0,size)"""
-    
 
 
 #ship_generator(random_number(int(input("Insert size: "))),random_number(int(input("Insert size: "))))
@@ -126,7 +103,7 @@ class TheBoards:
 #print(my_guesses)
 
 #create_board(3)
-print(create_board(3))
+#print(create_board(3))
 
 
 
